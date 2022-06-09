398465d1d270183b256f4b68d52c38b4d16a27ec
matsuo <matsuokuniko7@gmail.com>
2022-06-09 10:51:22 +0900

    crud

---
+++ crud/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    # 新規登録・編集完了後のリダイレクト先
+    def get_absolute_url(self):
+        return reverse('crud:list')
---
+++ crud/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = "crud"
+urlpatterns = [
+    # 一覧
+    path("", views.ProductListView.as_view(), name="list"),
+    # 新規登録
+    path('new/', views.ProductCreateView.as_view(), name='new'),
+    # 編集
+    path('edit/<int:pk>/', views.ProductUpdateView.as_view(), name='edit'),
+    # 削除
+    path('delete/<int:pk>/', views.ProductDeleteView.as_view(), name='delete'),
+]
